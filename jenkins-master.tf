2563c22e7274a92430ae65053d7f37699c7c9c6b
Maciej Piatek <magic@2fd5.com>
2020-11-11 14:27:30 +0000

    Example on creating multiple jobs

--- jenkins-master.tf
+++ jenkins-master.tf
@@ -13,14 +13,28 @@ resource jenkins_folder foo_folder {
   name = "moduleFoo"
 }
 
-resource jenkins_job foo_job {
-  name     = "${jenkins_folder.foo_folder.name}/BuildFoo"
+resource jenkins_job simple_job {
+  name     = "${jenkins_folder.foo_folder.name}/BuildSimple"
   template = file("${path.module}/job.xml")
 
   parameters = {
     branch_spec = "*/main"
     script_path = "Jenkinsfile"
     description = "An example job created from Terraform"
+    git_url = "https://github.com/2fd5/jenkins-master-template.git"
+  }
+}
+
+
+resource jenkins_job foos_job {
+  count = 5
+  name     = "${jenkins_folder.foo_folder.name}/BuildFoos-${count.index}"
+  template = file("${path.module}/job.xml")
+
+  parameters = {
+    branch_spec = "*/main"
+    script_path = "Jenkinsfile"
+    description = "An example job created from Terraform\ncount.index=${count.index}"
     git_url = "https://github.com/2fd5/jenkins-master-template.git"
   }
 }
